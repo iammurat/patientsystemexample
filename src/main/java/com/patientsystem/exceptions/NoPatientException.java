08d27e83a4d922ffab51e028af31c461b7e1a466
Murat Öz <muratoz85@gmail.com>
2019-10-15 23:01:03 +0300

    Exceptions mapped to ResponseStatus and reason
    Signed-off-by: Murat Öz <muratoz85@gmail.com>

--- src/main/java/com/patientsystem/exceptions/NoPatientException.java
+++ src/main/java/com/patientsystem/exceptions/NoPatientException.java
@@ -1,5 +1,9 @@
 package com.patientsystem.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus( value = HttpStatus.NOT_FOUND, reason = "Bad request!" )
 public class NoPatientException extends BusinessException
 {
 	public NoPatientException()
